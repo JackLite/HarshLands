548fd247ee5ee4d5a25b3e37b7026b2d83bf22ef
JackLite <jackkdlite@gmail.com>
2021-08-12 23:13:45 +0300

    Базовый проект + тест перемещения через новую Input System

---
+++ Assets/Tests/Moving/InputTest.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTest : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Vector3 _force;
+    public float speed = 5f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void ProcessInput(InputAction.CallbackContext context)
+    {
+        var value = context.ReadValue<Vector2>();
+        _force = new Vector3(value.x, 0, value.y);
+        Debug.Log( _force);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(_force * speed);
+    }
+}
